018227f863dd8130a2a197dc7f90b1dda8c9c2aa
mhenriette <mhenriette00@gmail.com>
2023-06-08 11:57:16 +0200

    fix: improve factorial and matrixSun

--- factorial.js
+++ factorial.js
@@ -1,9 +1,5 @@
 /* eslint-disable no-unused-vars */
 const calculateFactorial = (n) => {
-  if (n === 0 || n === 1) return 1;
-  let factorial = 1;
-  for (let i = 2; i <= n; i++) {
-    factorial *= i;
-  }
-  return factorial;
+  if (n <= 1) return 1;
+  return n * calculateFactorial(n - 1);
 };
